202db57059a0735b493e09b0e4ac273d419dd818
Nitzeli <ntzeli_alejandra@hotmail.com>
2020-08-06 21:37:11 -0500

    card

--- src/App.js
+++ src/App.js
@@ -34,27 +34,7 @@ function App() {
         <Clock />
         <List />
       </header>
-      <CardColumns>
-        <Post
-          img="https://i.pinimg.com/originals/81/02/68/81026878273e4c933692657e4eafbed3.jpg"
-          tittle="Un perrito"
-          subtittle="Samoyedo"
-          text="Esta raza, que toma el nombre del pueblo Samoyedo, es nativo de Siberia. Son los perros pastores por excelencia de la región. Aunque son criados en su región para este trabajo, También destacan por su belleza. En Europa es conocido como Bielkier"
-        />
-        <Post
-          img="https://dogbreeds10.com/wp-content/uploads/2018/11/Caucasian-Ovcharka-Full-Grown-200x200.jpg"
-          tittle="Otro perrito"
-          subtittle="Ovcharka"
-          text="Sus maneras y poses de lobo marcan la diferencia con el resto de sabuesos. Destaca por su rapidez, sobre todo, porque mientras otros sabuesos zigzaguean para dar alcance a su presa, el Borzoi les da alcance gracias a su velocidad punta"
-        />
-        <Post
-          img="https://www.ecured.cu/images/2/25/Mstor1.jpg"
-          tittle="Más perritos"
-          subtittle="Perro guardián moscovita"
-          text="Como curiosidad, este perro fue creado por orden de Joseph Stalin, en las instalaciones de Krásnaia Zvezda, con el fin de crear el perro policía perfecto. Se adapta a cualquier tipo de clima, resistente y muy obediente, si se le entrena bien.
-          Pastor de Europa del Este"
-        />
-      </CardColumns>
+      <Post />
     </div>
   );
 }
