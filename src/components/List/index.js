94bccae02899422c727ca5ea6a0ff482eb16b3f8
Nitzeli <ntzeli_alejandra@hotmail.com>
2020-08-06 22:06:33 -0500

    hide button :bug:

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,34 +1,72 @@
 import React, { Component } from "react";
-import { Container, Row, ListGroup, ListGroupItem, Col } from "reactstrap";
+
+// CSS
 import "./List.css";
+import {
+  Container,
+  Col,
+  Row,
+  ListGroupItem,
+  ListGroup,
+  Badge,
+  Button,
+} from "reactstrap";
 
-const elements = [
-  {
-    position: 1,
-    text: "perro",
-  },
-  {
-    position: 2,
-    text: "text 2",
-  },
-  {
-    position: 3,
-    text: "text 3",
-  },
-];
-
+const elements = [{ text: "Text 1" }, { text: "dinamic" }, { text: "Text 3" }];
+// const elements = [];
 class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeList: true,
+    };
+    this.handleShowListClick = this.handleShowListClick.bind(this);
+  }
+
+  handleShowListClick() {
+    const { activeList } = this.state;
+    this.setState({
+      activeList: !activeList,
+    });
+  }
+
   render() {
-    const UIElements = elements.map(({ text }) => (
-      <ListGroupItem className="List-item">{text}</ListGroupItem>
-    ));
+    const { activeList } = this.state;
+
+    let UIElements = elements.map(({ text }, index) => {
+      const upperText = text.toUpperCase();
+      return (
+        <ListGroupItem
+          onClick={this.handleListItemClick}
+          key={index}
+          className="List-item"
+        >
+          {upperText}
+        </ListGroupItem>
+      );
+    });
+
+    /*
+      UIElements -> [] -> have a length property
+      if (!UIElements.length) -> if(!0) -> if(!false)
+      if (!false) -> if (true)
+    */
+
+    if (!UIElements.length) {
+      UIElements = (
+        <h1>
+          No hay <Badge color="danger">elementos</Badge>
+        </h1>
+      );
+    }
 
     return (
       <Container>
         <Row>
-          <Col>
-            <ListGroup>{UIElements}</ListGroup>
-          </Col>
+          <Col>{activeList ? <ListGroup>{UIElements}</ListGroup> : null}</Col>
+          <Button onClick={this.handleShowListClick}>
+            {activeList ? "Hide list" : "Show List"}
+          </Button>
         </Row>
       </Container>
     );
