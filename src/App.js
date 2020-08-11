96b9831bb788083eb90460f8b517a9e0925ec938
Nitzeli <ntzeli_alejandra@hotmail.com>
2020-08-10 20:10:43 -0500

    clase 6

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-// Components
-
-import FunctionGreet from "./components/FunctionGreet";
-import ClassGreet from "./components/ClassGreet";
-import Post from "./components/Post";
-import { Container, CardColumns } from "reactstrap";
-import Clock from "./components/Date";
-import SwitchExample from "./components/SwitchExample";
-import List from "./components/List";
+// Screens
+import BlogNotes from "./screens/NotesBlog";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <FunctionGreet firstName="Nitz" lastName="Villa" />
-        <ClassGreet firstName="Otra vez Nitz" lastName=":3 " />
-        <SwitchExample />
-        <Clock />
-        <List />
-      </header>
-      <Post />
+      <BlogNotes />
     </div>
   );
 }
